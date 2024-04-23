b567f22f5f59f8ab622300ebb2735f9e0995c29c
trinketmage <tranremi@live.fr>
2024-04-23 17:21:58 +0200

    Highlight > base sticky

--- src/views/Home/HighlightView.vue
+++ src/views/Home/HighlightView.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="view">
-    <div
-      class="card"
-      v-for="({ id, color, title }) in movies"
-      :key="id"
-      :style="{
-        backgroundColor: color
-      }"
-    >
-    {{ title }}
+  <div class="view" ref="view">
+    <div class="scroll-helper">
+      <div
+        class="card"
+        v-for="({ id, color, title }) in movies"
+        :key="id"
+        :style="{
+          backgroundColor: color
+        }"
+      >
+      {{ title }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Movies from "@/assets/movies.json";
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
+import ScrollTrigger from "@gsap/shockingly/ScrollTrigger";
 
 export default {
   setup() {
@@ -30,8 +33,29 @@ export default {
       })
     );
 
+    const view = ref(null);
+    let st = null;
+
+    onMounted(() => {
+
+      st = ScrollTrigger.create({
+        trigger: view.value.querySelector('.scroll-helper'),
+        start: "top top", 
+        end: () => "+=200%",
+        pin: true,
+        scrub: true,
+        markers: true
+        // animation,
+      });
+    })
+
+    onUnmounted(() => {
+      if (st) st.kill();
+    })
+
     return {
-      movies
+      movies,
+      view
     }
   }
 }
@@ -39,7 +63,11 @@ export default {
 
 <style lang="scss" scoped>
 .view {
+  height: 300vh;
+}
+.scroll-helper {
   overflow: hidden;
+  height: 100vh;
 }
 .card {
   height: 100vh;

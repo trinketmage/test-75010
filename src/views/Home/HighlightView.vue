e411f7a119802f48f643494f979d0908ad9672a9
trinketmage <tranremi@live.fr>
2024-04-23 16:25:03 +0200

    Highlight > bae

--- src/views/Home/HighlightView.vue
+++ src/views/Home/HighlightView.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="view">
-    Highlight
+    <div
+      class="card"
+      v-for="({ id, color, title }) in movies"
+      :key="id"
+      :style="{
+        backgroundColor: color
+      }"
+    >
+    {{ title }}
+    </div>
   </div>
 </template>
 
 <script>
+import Movies from "@/assets/movies.json";
+import { ref } from "vue";
+
 export default {
   setup() {
+    let ids = [
+      "film-c-1",
+      "film-c-2",
+      "film-c-3"
+    ];
+    const movies = ref(
+      Movies.filter(({ id }) => {
+        return ids.indexOf(id) > -1;
+      })
+    );
+
+    return {
+      movies
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.view {
+  overflow: hidden;
+}
+.card {
+  height: 100vh;
+}
 </style>

adcd31be902079d22b6630d49d6de5c6ea8bc779
trinketmage <tranremi@live.fr>
2024-04-24 17:22:31 +0200

    StarLink > update

--- src/components/StarLink.vue
+++ src/components/StarLink.vue
@@ -1,10 +1,11 @@
 <template>
     <a class="star-link" @click="handleClick" ref="link">
-        ⭐️ PICK OF THE MONTH
+        ⭐️ <span class="label">PICK OF THE MONTH</span>
     </a>
 </template>
 
 <script>
+import gsap from "@gsap/shockingly";
 import { ref } from "vue";
 
 export default {
@@ -16,7 +17,40 @@ export default {
     setup() {
         const link = ref(null);
         const handleClick = () => {
-            console.log('click');
+            const stars = [];
+            for (var i = 0; i < 10; i++) {
+                const star = document.createElement("div");
+                star.setAttribute("class", "star");
+                star.innerHTML = "⭐️";
+                link.value.appendChild(star);
+                stars.push(star);
+            }
+            gsap.set(stars, {
+                scale: "random(0.4, 1)",
+            });
+            gsap.timeline({
+                onComplete: () => {
+                    if (link.value) {
+                        stars.forEach(star => {
+                            link.value.removeChild(star);
+                        });
+                    }
+                }
+            })
+                .to(stars, {
+                    duration: 1.5,
+                    physics2D: {
+                        velocity: "random(200, 450)",
+                        angle: "random(250, 290)",
+                        gravity: 500
+                    },
+                    delay: "random(0, 1.0)"
+                }, 'a')
+                .to(stars, {
+                    alpha: 0,
+                    duration: 0.3
+                }, '-=0.3');
+
         }
 
         return {
@@ -32,5 +66,46 @@ export default {
     font-size: 24px;
     font-weight: bold;
     cursor: pointer;
+    position: relative;
+    display: inline-block;
+    perspective: 800px;
+    user-select: none;
+
+    .label {
+        display: inline-block;
+        transition: transform .3s ease;
+    }
+
+    &:before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0px;
+        right: 0;
+        bottom: 0px;
+        left: 30px;
+        background-color: var(--midnight);
+        z-index: -1;
+        transform: scaleX(0) rotate(0deg);
+        transform-origin: 25% 50%;
+        transition: transform .3s ease;
+    }
+
+    &:deep(.star) {
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 0px;
+        z-index: -1;
+    }
+
+    &:hover {
+        &:before {
+            transform: scaleX(1) rotate(2deg);
+        }
+        .label {
+            transform: translateZ(50px) rotateY(-12.5deg);
+        }
+    }
 }
 </style>

8ace78bd47278bb6ae054419278eee72adf71659
trinketmage <tranremi@live.fr>
2024-04-24 16:30:48 +0200

    Header > animation

--- src/views/HeaderView.vue
+++ src/views/HeaderView.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="header gabarit">
-        <a><RessortGraphique />MENU</a>
+        <a class="main-link"><RessortGraphique /><span class="copy">MENU</span></a>
         <div class="fast-nav">
-            <a>POPULAR</a>
-            <a>NOW SHOWING</a>
-            <a>WATCHLIST</a>
+            <a class="underline">POPULAR</a>
+            <a class="underline">NOW SHOWING</a>
+            <a class="underline">WATCHLIST</a>
         </div>
     </div>
 </template>
@@ -37,5 +37,51 @@ a {
     display: flex;
     align-items: center;
     justify-content: center;
+    padding-top: 4px;
+    padding-bottom: 4px;
 }
+.main-link {
+
+    .copy {
+        transition: transform .3s ease;
+        position: relative;
+        z-index: 10;
+    }
+    .ressort-graphique :deep(.circle) {
+        transition: transform .3s ease;
+        &:nth-child(3) {
+            transform: scaleY(-1);
+        }
+    }
+    &:hover {
+        .copy {
+            transform: translateX(-7px);
+        }
+        .ressort-graphique :deep(.circle) {
+            &:nth-child(1) {
+                transform: rotate(260deg) translateY(65px) rotate(-0deg);
+            }
+            &:nth-child(2) {
+                transform: translateX(-9px) translateY(-10px);
+            }
+            &:nth-child(3) {
+                transform: scaleY(-1) rotate(260deg) translateY(65px) rotate(-0deg) ;
+            }
+            &:nth-child(4) {
+                transform: translateX(-9px) translateY(10px);
+            }
+        }
+    }
+}
+
+  
+@keyframes GFG { 
+    0% { 
+        transform: rotate(0deg) translateY(100px) rotate(0deg); 
+    } 
+
+    100% { 
+        transform: rotate(360deg) translateY(100px) rotate(-360deg); 
+    } 
+} 
 </style>
